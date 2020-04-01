e7a27a9c4e062e2f7cdde217ea08b0f0ea224b0a
prateekdhall@gmail.com <prateekdhall@gmail.com>
2020-04-01 23:15:13 +0530

    [Assignment5-DataWeave Functions]

---
+++ src/main/java/dataweaveassignment5/Travel.java
@@ -0,0 +1,29 @@
+package dataweaveassignment5;
+
+public class Travel {
+
+	private String destination;
+	private double price;
+	private String planeType;
+	
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getPlaneType() {
+		return planeType;
+	}
+	public void setPlaneType(String planeType) {
+		this.planeType = planeType;
+	}
+
+	
+}
